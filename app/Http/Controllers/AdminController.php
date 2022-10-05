b64e7fa156778003becf9e7ae53d343102344455
Samir Karki <karkisamir99@gmail.com>
2022-10-05 23:51:38 +0545

    Multi-authentication fully working

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function authenticate(Request $request){
+        // dd($request->all());
+        $attributes=$this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if(Auth::guard('admin')->attempt($attributes, $request->get('remember'))){
+            return redirect()->route('admin.dashboard')->with('success','You are successfully logged in.');
+        }else{
+            return back()->withInput($request->only('email'))->with('error', 'Credentials does not match!');
+        }
+    }
+
+    public function dashboard(){
+        return view('admin.dashboard');
+    }
+
+    function logout(){
+         Auth::guard('admin')->logout();
+         return redirect()->route('admin.login');
+    }
+}
+     
